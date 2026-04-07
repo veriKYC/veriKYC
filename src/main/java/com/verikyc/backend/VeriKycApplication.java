7ddba2f40c945f340b56312113a129b6a4ca34c7
rajdeepthaosen7 <rajdeepthaosen7@gmail.com>
2026-04-08 02:07:36 +0530

    first commit

---
+++ src/main/java/com/verikyc/backend/VeriKycApplication.java
@@ -0,0 +1,13 @@
+package com.verikyc.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeriKycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeriKycApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/verikyc/backend/VeriKycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verikyc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeriKycApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
